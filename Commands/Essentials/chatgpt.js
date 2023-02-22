0876519b843006c22a6c81dfa453009bb6052bef
git config --global user.name Fazil <105953406+mu-fazil-vk@users.noreply.github.com>
2023-02-22 20:49:36 +0530

    gpt help

--- Commands/Essentials/chatgpt.js
+++ Commands/Essentials/chatgpt.js
@@ -14,7 +14,7 @@ module.exports = {
         if (!args[0])
           return Miku.sendMessage(
             m.from,
-            { text: `Please provide message/question!\n\n_eg: ${prefix}gpt write about earth` },
+            { text: `Please provide message/question!\n\n_eg: ${prefix}gpt write about earth_` },
             { quoted: m }
           );
 
